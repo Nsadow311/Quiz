dca17367482f0ae40d9f2f0020677a0c25e258cb
Nsadow311 <63483406+Nsadow311@users.noreply.github.com>
2021-02-23 03:51:11 +0000

    Add files via upload

---
+++ mbr_config.lua
@@ -0,0 +1,13 @@
+MBR = MBR or {}
+MBR.config = MBR.config or {}
+
+local config = MBR.config
+
+config.main = {
+    ["rp_stardestroyer_v2_5"] = {
+        ["2232"] = {
+            teams = { ["Gun Dealer"] = true },
+            usergroups = { ["superadmin"] = true, ["admin"] = true },
+        },
+    },
+}
